d209dc461fd9d4812fb2033faf7dfb5fb05a7e08
MDABDULMALIK121 <abdulmalikjmi6@gmail.com>
2022-03-05 18:13:34 +0530

    Add project files.

---
+++ CASECADING_1/Models/State.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CASECADING_1.Models
+{
+    public class State
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Country Country { get; set; }
+    }
+}
